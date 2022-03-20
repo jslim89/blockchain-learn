5143e9005691751841528083118cbbd6cece8e5a
Js Lim <jslim89@gmail.com>
2022-03-20 16:07:36 +0800

    finalize brownie_fund_me

--- brownie-smart-contract/src/brownie_fund_me/scripts/helpful_scripts.py
+++ brownie-smart-contract/src/brownie_fund_me/scripts/helpful_scripts.py
@@ -4,21 +4,24 @@ from web3 import Web3
 
 DECIMALS = 8
 STARTING_PRICE = 200000000000
-LOCAL_BLOCKCHAIN_ENV = ['dev', 'ganache-local']
+FORK_LOCAL_ENV = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENV = ["dev", "ganache-local"]
+
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENV:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENV
+        or network.show_active() in FORK_LOCAL_ENV
+    ):
         return accounts[0]
     else:
-        return accounts.add(config['wallets']['from_key'])
-    
+        return accounts.add(config["wallets"]["from_key"])
+
 
 def deploy_mock():
-    print(f'active network is {network.show_active()}')
-    print('Deploying mock')
+    print(f"active network is {network.show_active()}")
+    print("Deploying mock")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(
-            DECIMALS, STARTING_PRICE, {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
     print(MockV3Aggregator[-1].address)
-    print('mock deployed')
+    print("mock deployed")
